315c1ab659d57b399adf2fef0561fd07f1957df1
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2022-12-03 08:42:20 -0400

    feat: add api return to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,60 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Header from "../components/header/header"
-import DailyCoin from "../components/dailyCoin/dailyCoin"
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Header from "../components/header/header";
+import DailyCoin from "../components/dailyCoin/dailyCoin";
+import { GetStaticProps } from "next";
+import { api } from "./api/api";
 
-export default function Home() {
+interface IProducts {
+  id: number;
+  name: string;
+  description: string;
+  img_url: string;
+  price: number;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface HomeProps {
+  products: IProducts[];
+}
+
+export default function Home({ products }: HomeProps) {
   return (
     <>
       <Head>
         <title>Brypto</title>
       </Head>
       <div className="body">
-
         <Header />
+        <div className={styles.title}>
+          <h1>
+            Olá, seja bem vindo a <span className={styles.bryto}>Brypto!</span>
+          </h1>
+        </div>
 
-       <div className={styles.title}> <h1>Olá, seja bem vindo a <span className={styles.bryto}>Brypto!</span></h1> </div>
+        <div className={styles.day}>Day crypto</div>
 
-       <div className={styles.day}>Day crypto</div>
+        <DailyCoin />
 
-       <DailyCoin />
+        {products.map((product) => {
+          return(
+          <h1>{product.name}</h1>
+          )
+        })}
       </div>
     </>
   );
 }
 
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await api.get("products");
+  const productsList: IProducts[] = response.data;
+
+  return {
+    props: {
+      products: productsList,
+    },
+    revalidate: 10,
+  };
+};
