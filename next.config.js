f0df00c09b87f82e5e7884b64aaba874846cf3ec
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2022-12-04 18:40:49 -0400

    chore: update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')
+const isProd = process.env.NODE_ENV === 'production'
 
-module.exports = nextConfig
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: !isProd
+  },
+})
