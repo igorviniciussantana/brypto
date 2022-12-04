69b6df0fcfb384a3dae4f3127e222f663befa53c
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2022-12-04 18:30:57 -0400

    feat(my_wallet): add hide view

--- components/balance/balance.tsx
+++ components/balance/balance.tsx
@@ -1,11 +1,15 @@
 import Head from "next/head";
+import { useState } from "react";
 import styles from "./balance.module.css";
 
 export default function Balance() {
+
+  const [coinView, setCoinView ]= useState(false)
+  
   return (
     <div className={styles.balance}>
-        <div className={styles.line}><p>MEU SALDO</p> <img src="img/eye.svg" alt="Icone do olho" /></div>
-        <div className={styles.coin}><h1>R$ 99999,99</h1></div>
+        <div className={styles.line}><p>MEU SALDO</p> <img src="img/eye.svg" alt="Icone do olho"  onClick={() => coinView ? setCoinView(false) :  setCoinView(true)}/></div>
+        <div className={styles.coin} ><h1>{coinView ? 'R$ 99999,99' : '*********'}</h1></div>
 
         <div className={styles.over}>
             <p>Depósito</p>
